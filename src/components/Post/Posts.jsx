0b0610637602a4e9ab317e49c849cf55a7952353
shjroemon <zzdaotuanhuyzz@gmail.com>
2023-09-17 13:48:44 +0700

    fix

--- src/components/Post/Posts.jsx
+++ src/components/Post/Posts.jsx
@@ -3,11 +3,15 @@ import Loading from "./Loading";
 import Post from "./Post";
 
 const Posts = ({ posts, isLoading }) => {
-	return (
-		<div className="posts">
-			{isLoading ? <Loading /> : posts.map(post => <Post post={post} key={post._id} />)}
-		</div>
-	);
+  return (
+    <div className="posts">
+      {isLoading ? (
+        <Loading />
+      ) : (
+        posts.map((post) => <Post post={post} key={post._id} />)
+      )}
+    </div>
+  );
 };
 
 export default Posts;
